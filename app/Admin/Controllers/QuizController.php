363fc016076c09ea8d98301a956ea0b92eafb027
Masato <project-europa@excite.co.jp>
2017-10-11 14:24:57 +0900

    add form join other Model Class.

--- app/Admin/Controllers/QuizController.php
+++ app/Admin/Controllers/QuizController.php
@@ -4,6 +4,7 @@ namespace App\Admin\Controllers;
 
 use App\Quiz;
 use App\Category;
+use App\Answer;
 
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
@@ -77,7 +78,11 @@ class QuizController extends Controller
             $grid->id('ID')->sortable();
             $grid->title();
             $grid->answers_id();
-            $grid->categories_id();
+            $grid->category()->name('cateogry Name')->sortable();
+            $grid->answer()->answer_1();
+            $grid->answer()->answer_2();
+            $grid->answer()->answer_3();
+            $grid->answer()->answer_4();
             $grid->image_src();
             $grid->created_at();
             $grid->updated_at();
@@ -91,32 +96,60 @@ class QuizController extends Controller
      */
     protected function form()
     {
+
         return Admin::form(Quiz::class, function (Form $form) {
+
+            $answersLatestId = Answer::latest('id')->first();
+            // Answerテーブル最新IDの次のID
+            $answersLatestNextId = $answersLatestId->id + 1;
 
             $form->display('id', 'ID');
             $form->text('title')->rules('required|max:190');
-            $form->select('answer_id')->options(function () {
-                $quiz = Quiz::latest('id')->first();
-            
-                if ($quiz) {
-                    return [$quiz->id => $quiz->id];
+            $form->file('image_src')->removable();
+            $form->select('answers_id')->options(function () {
+                $answersIds = Answer::select('id')->get();
+                $answersIdsArray = array();
+                $count = count($answersIds);
+                foreach($answersIds as $answersId) {
+                    $answersIdsArray[$answersId->id] = $answersId->id;
+                    
+                    if ($count == $answersId->id) {
+                        // foreachが最後であれば、Answerテーブルの最新IDの次のIDを追加する
+                        $answersIdsArray[$answersId->id + 1] = $answersId->id + 1;
+                    }
                 }
-            });
-            $form->select('answer_id')->options(function () {
-                $quiz = Quiz::latest('id')->first();
-            
-                if ($quiz) {
-                    return [$quiz->id => $quiz->id];
+                if ($answersIdsArray) {
+                    return $answersIdsArray;
                 }
-            });
+            })->default($answersLatestNextId)->rules('required');// デフォルト値はAnswerテーブル最新IDの次のID
+            $form->select('categories_id')->options(function () {
+                $categories = Category::select('id', 'name')->get();
+
+                $categoryArray = array();
+
+                foreach($categories as $category) {
+                    $categoryArray[$category->id] = $category->name;
+                }
+            
+                if ($categoryArray) {
+                    return $categoryArray;
+                }
+            })->rules('required');
+            $form->text('answer.answer_1', 'answer_1')->rules('required');
+            $form->text('answer.answer_2', 'answer_2')->rules('required');
+            $form->text('answer.answer_3', 'answer_3')->rules('required');
+            $form->text('answer.answer_4', 'answer_4')->rules('required');
+            $form->select('answer.correct_answer_no', 'correctAnswerNo')->options(function () {
+                $correctAnswerNoArray = array();
+                // 4択問題なので、1-4のセレクトボックスを生成
+                for($i = 1; $i <= 4; $i++) {
+                    $correctAnswerNoArray[$i] = $i;
+                }
+                return $correctAnswerNoArray;
+            })->rules('required');
+            $form->textarea('answer.commentary', 'Commentary')->rules('required');
             $form->display('created_at', 'Created At');
             $form->display('updated_at', 'Updated At');
         });
-    }
-    public function users(Request $request)
-    {
-        $q = $request->get('q');
-    
-        return User::where('name', 'like', "%$q%")->paginate(null, ['id', 'name as text']);
     }
 }
--- app/Quiz.php
+++ app/Quiz.php
@@ -2,9 +2,19 @@
 
 namespace App;
 
+use App\Answer;
+use App\Category;
 use Illuminate\Database\Eloquent\Model;
 
 class Quiz extends Model
 {
-    //
+    public function answer()
+    {
+        return $this->hasOne(Answer::class, 'id', 'answers_id');
+    }
+
+    public function category()
+    {
+        return $this->hasOne(Category::class, 'id', 'categories_id');
+    }
 }
