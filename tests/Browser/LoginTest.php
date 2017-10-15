64401250e2794b6269a325c784d9958b07a1ab26
Masato <project-europa@excite.co.jp>
2017-10-15 14:05:05 +0900

    Install Laravel Dusk.

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\User;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class LoginTest extends DuskTestCase
+{
+    /**
+     * ログインテスト.
+     *
+     * @return void
+     */
+    public function testLogin()
+    {
+        $user = factory(User::class)->create();
+
+        $this->browse(function ($browser) use ($user){
+            $browser->visit('/login')
+                    ->type('email', $user->email)
+                    ->type('password', 'secret')
+                    ->press('ログイン')
+                    ->assertPathIs('/');
+        });
+    }
+}
